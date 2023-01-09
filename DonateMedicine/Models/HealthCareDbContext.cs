da05d05418447026c3839cbcab2d9804c9aaca83
vmadmin <vmadmin@Dotnet>
2023-01-09 11:38:09 +0000

    Add project files.

---
+++ DonateMedicine/Models/HealthCareDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonateMedicine.Models
+{
+    public class HealthCareDbContext : DbContext
+    {
+        public HealthCareDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DonateMedicine/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonateMedicine.Models
+{
+    public class Register
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string  Address { get; set; }
+    }
+}
